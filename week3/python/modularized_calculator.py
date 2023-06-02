73f69289938252e6dc4b450495fc43da457ae0bb
yomek33 <i3landme@gmail.com>
2023-06-02 15:34:57 +0900

    week3

--- week3/python/modularized_calculator.py
+++ week3/python/modularized_calculator.py
@@ -12,6 +12,9 @@ def read_number(line, index):
             number += int(line[index]) * decimal
             decimal /= 10
             index += 1
+    elif index < len(line) and line[index] == '(':
+        index += 1
+        decimal = 0.1
     token = {'type': 'NUMBER', 'number': number}
     return token, index
 
@@ -32,6 +35,14 @@ def read_division(line, index):
     token = {'type': 'DIVISION'}
     return token, index + 1
 
+def read_left_bracket(line, index):
+    token = {'type': 'LEFT_BRACKET'}
+    return token, index + 1
+
+def read_right_bracket(line, index):
+    token = {'type': 'RIGHT_BRACKET'}
+    return token, index + 1
+
 def tokenize(line):
     tokens = []
     index = 0
@@ -46,18 +57,32 @@ def tokenize(line):
             (token, index) = read_plus(line, index)
         elif line[index] == '-':
             (token, index) = read_minus(line, index)
+        elif line[index] == '(':
+            (token, index) = read_left_bracket(line, index)
+        elif line[index] == ')':
+            (token, index) = read_right_bracket(line, index)
         else:
             print('Invalid character found: ' + line[index])
             exit(1)
         tokens.append(token)
     return tokens
 
-
 def evaluate(tokens):
     answer = 0
     tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
     index = 1
     while index < len(tokens):
+        if tokens[index]['type'] == 'LEFT_BRACKET':
+            bracket_count = 1
+            for i in range(index+1, len(tokens)):
+                if tokens[i]['type'] == 'LEFT_BRACKET':
+                    bracket_count += 1
+                elif tokens[i]['type'] == 'RIGHT_BRACKET':
+                    bracket_count -= 1
+                if bracket_count == 0:
+                    sub_answer = evaluate(tokens[index+1:i])
+                    tokens = tokens[:index] + [{'type': 'NUMBER', 'number': sub_answer}] + tokens[i+1:]
+                    return evaluate(tokens)
         if tokens[index]['type'] == 'NUMBER':
             if tokens[index - 1]['type'] == 'MULTIPLICATION':
                 answer *= tokens[index]['number']
@@ -67,10 +92,6 @@ def evaluate(tokens):
                 answer += tokens[index]['number']
             elif tokens[index - 1]['type'] == 'MINUS':
                 answer -= tokens[index]['number']
-                
-            else:
-                print('Invalid syntax')
-                exit(1)
         index += 1
     return answer
 
@@ -94,6 +115,15 @@ def run_test():
     test("3*4/2")
     test("3/2/2/19")
     print("==== Test finished! ====\n")
+#==== Test started! ====
+# PASS! (1+2 = 3.000000)
+# PASS! (1.0+2.1-3 = 0.100000)
+# PASS! (0 = 0.000000)
+# PASS! (3*4/2 = 6.000000)
+# PASS! (3/2/2/19 = 0.039474)
+# ==== Test finished! ====
+
+
 
 run_test()
 
